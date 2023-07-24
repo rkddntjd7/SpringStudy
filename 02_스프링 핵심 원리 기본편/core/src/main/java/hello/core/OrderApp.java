bd2a471e325c6c08c69b26310f67dc9be9b73ba2
rkddntjd7 <125332196+rkddntjd7@users.noreply.github.com>
2023-07-25 01:58:10 +0900

    Ioc, DI 컨테이너 까지

--- 02_스프링 핵심 원리 기본편/core/src/main/java/hello/core/OrderApp.java
+++ 02_스프링 핵심 원리 기본편/core/src/main/java/hello/core/OrderApp.java
@@ -8,16 +8,19 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+//        MemberService memberService = new MemberServiceImpl();
+//        OrderService orderService = new OrderServiceImpl();
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         System.out.println("order = " + order);
-        System.out.println("order = " + order.calculatePrice());
+
     }
 }
