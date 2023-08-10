9add55b42cb29bbc4290259a2d8bb5120c41ccf8
rkddntjd7 <125332196+rkddntjd7@users.noreply.github.com>
2023-08-11 01:57:40 +0900

    유연한 컨트롤러2 - v5

---
+++ 03_스프링 MVC 1편/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,14 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @param paramMap
+ * @param model
+ * @return viewName
+ */
+public interface ControllerV4 {
+
+    String process(Map<String, String> paramMap, Map<String, Object> model);
+}
